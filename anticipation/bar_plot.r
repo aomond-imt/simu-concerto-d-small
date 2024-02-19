7672dfc76e736d97564a15818170a34a5fdeed14
aomond <antoine.omond@imt-atlantique.fr>
2024-02-19 17:53:27 +0100

    plot results

--- anticipation/bar_plot.r
+++ anticipation/bar_plot.r
@@ -3,9 +3,14 @@ library("dplyr")
 
 d <- read.csv(glue::glue("aggregated_results.csv"))
 d<-d %>% filter(nb_deps_seq == 5)
-ggplot(d, aes(x=chains_length, y=dynamic, fill=type_comms, label=sprintf("%0.2f", dynamic))) +
-    geom_bar(color="black", stat = "identity", position = "dodge") +
-    geom_text(aes(y=min(dynamic)/2), fill="white", fontface="bold", position=position_dodge(width=0.9)) +
+d<-d %>% filter(is_pipeline == "False")
+d<-d %>% filter(nb_chains == 1)
+d<-d %>% filter(topology == "clique")
+dodge <- 0.9
+ggplot(d, aes(x=chains_length, y=duration_mean, fill=type_comms, label=sprintf("%0.2f", duration_mean))) +
+    geom_bar(color="black", stat = "identity", position= "dodge") +
+    geom_errorbar(position=position_dodge(dodge), aes(ymin=duration_mean - duration_std, ymax=duration_mean + duration_std), width=.2, linewidth=1) +
+    geom_text(aes(y=min(duration_mean)/2), fill="white", fontface="bold", position=position_dodge(width=dodge)) +
     ylab(glue::glue("Accumulated energy consumption (J)")) +
     theme(axis.text.y = element_text()) +
     xlab(element_blank()) +
