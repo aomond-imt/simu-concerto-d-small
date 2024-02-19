f639f07929ac1281f219c2c965cf800dd452ccc8
aomond <antoine.omond@imt-atlantique.fr>
2024-02-19 17:32:53 +0100

    fix agg res

--- anticipation/aggregate_results.py
+++ anticipation/aggregate_results.py
@@ -6,7 +6,7 @@ vals_to_aggregate = {}
 with open("results.csv") as f:
     reader = csv.DictReader(f)
     for r in reader:
-        key = (r["type_comms"], r["nb_deps_seq"], r["deps_pos"], r["chains_length"], r["nb_chains"], r["topology"])
+        key = (r["type_comms"], r["nb_deps_seq"], r["chains_length"], r["is_pipeline"], r["nb_chains"], r["topology"])
         vals_to_aggregate.setdefault(key, {"static": [], "dynamic": [], "duration": []})["static"].append(float(r["static"]))
         vals_to_aggregate[key]["dynamic"].append(float(r["dynamic"]))
         vals_to_aggregate[key]["duration"].append(float(r["duration"]))
@@ -15,13 +15,15 @@ with open("results.csv") as f:
         csvwriter = csv.writer(f, delimiter=",")
         csvwriter.writerow([
             "static_mean", "static_std", "dynamic_mean", "dynamic_std", "duration_mean", "duration_std",
-            "type_comms", "nb_deps_seq", "deps_pos", "chains_length", "nb_chains", "topology"
+            "type_comms", "nb_deps_seq", "chains_length", "is_pipeline", "nb_chains", "topology", "nb_occs"
         ])
         for key in vals_to_aggregate.keys():
+            assert len(vals_to_aggregate[key]["static"]) == len(vals_to_aggregate[key]["dynamic"]) == len(vals_to_aggregate[key]["duration"])
             csvwriter.writerow([
                 round(np.asarray(vals_to_aggregate[key]["static"]).mean(), 2), round(np.asarray(vals_to_aggregate[key]["static"]).std(), 2),
                 round(np.asarray(vals_to_aggregate[key]["dynamic"]).mean(), 2), round(np.asarray(vals_to_aggregate[key]["dynamic"]).std(), 2),
                 round(np.asarray(vals_to_aggregate[key]["duration"]).mean(), 2), round(np.asarray(vals_to_aggregate[key]["duration"]).std(), 2),
-                *key
+                *key,
+                len(vals_to_aggregate[key]["static"])
             ])
 print("done")
