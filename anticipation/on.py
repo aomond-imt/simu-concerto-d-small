4229f1a5bc1d200e5015cc76533792eb6b4b7f5e
aomond <antoine.omond@imt-atlantique.fr>
2024-02-19 11:02:48 +0100

    Pull + tests

--- anticipation/on.py
+++ anticipation/on.py
@@ -14,7 +14,7 @@ COMMS_CONSO = 0.16
 SERVICE_NAME_SIZE = 16
 NODE_ID_SIZE = 16
 EXPORTED_VALUES_SIZE = 1_000  # High value to cover worst cases
-SERVICE_TABLE_ENTRY_SIZE = SERVICE_NAME_SIZE + NODE_ID_SIZE + EXPORTED_VALUES_SIZE
+COORDINATION_TABLE_ENTRY_SIZE = SERVICE_NAME_SIZE + NODE_ID_SIZE + EXPORTED_VALUES_SIZE
 REQUEST_SIZE = 257
 
 
@@ -27,11 +27,11 @@ def execute(api: Node):
     # Metrics
     upt_count, sleep_time, upt_time, stress_time, nb_msg_sent, nb_msg_rcv = 0, 0, 0, 0, 0, 0
 
-    service_table = {}
+    coordination_table = {}
     tasks_list = api.args["ons_tasks"][api.node_id]
     termination_list = api.args["termination_list"]
     uptimes_schedules = api.args["uptimes_schedules"][api.node_id]
-    data_to_fetch = set(d for d in tasks_list[0][2])
+    content_to_fetch = set(d for d in tasks_list[0][2])
     for upt in uptimes_schedules:
         # Sleeping period
         api.turn_off()
@@ -46,46 +46,84 @@ def execute(api: Node):
         api.turn_on()
         node_cons.set_power(IDLE_CONSO)
         upt_count += 1
-        api.send(INTERFACE_NAME, ("ping", data_to_fetch), PING_SIZE, 0)
+        api.send(INTERFACE_NAME, ("ping", None), PING_SIZE, 0)
         nb_msg_sent += 1
         upt_end = upt + UPT_DURATION
+
+        while len(tasks_list) > 0 and all(dep in coordination_table.keys() for dep in tasks_list[0][2]):
+            task_name, task_time, _ = tasks_list[0]
+            node_cons.set_power(STRESS_CONSO)
+            api.wait(task_time)
+            node_cons.set_power(IDLE_CONSO)
+            stress_time += task_time
+            tasks_list.pop(0)
+            coordination_table[task_name] = [api.node_id, {}]
+            if len(tasks_list) == 0:
+                termination_list[api.node_id] = True
+            else:
+                for task_name in tasks_list[0][2]:
+                    content_to_fetch.add(task_name)
+
         code, data = api.receivet(INTERFACE_NAME, timeout=max(0, upt_end - api.read("clock")))
         while data is not None:
             nb_msg_rcv += 1
             t, content = data
             api.log(f"content: {str(content)}")
-            if t == "ping":
-                if api.args["type_comms"] == "push":
-                    data_to_send = service_table
-                else:
-                    data_to_send = {task_name: val for task_name, val in service_table.items() if task_name in content}
+            if api.args["type_comms"] == "pull":
+                if t == "ping":
+                    contentsize = REQUEST_SIZE + len(content_to_fetch)*SERVICE_NAME_SIZE
+                    api.send(INTERFACE_NAME, ("ack", content_to_fetch), contentsize, 0)
+                if t == "ack":
+                    content_to_send = {task_name: val for task_name, val in coordination_table.items() if task_name in content}
+                    content_size = REQUEST_SIZE + COORDINATION_TABLE_ENTRY_SIZE * len(content_to_send.keys())
+                    content_size += len(content_to_fetch)*SERVICE_NAME_SIZE
+                    api.send(INTERFACE_NAME, ("resp", (content_to_send, content_to_fetch)), content_size, 0)
                     for task_name in content:
-                        if task_name not in service_table.keys():
-                            data_to_fetch.add(task_name)
-                api.log(f"data_to_send: {str(data_to_send)}")
-                api.log(f"data_to_fetch: {str(data_to_fetch)}")
-                content_size = REQUEST_SIZE + SERVICE_TABLE_ENTRY_SIZE * len(data_to_send.keys())
-                api.send(INTERFACE_NAME, ("resp", data_to_send), content_size, 0)
-                nb_msg_sent += 1
-            else:
-                service_table.update(content)
-                for task_name in service_table.keys():
-                    if task_name in data_to_fetch:
-                        data_to_fetch.remove(task_name)
-                api.log(f"service_table: {str(service_table)}")
-                if len(tasks_list) > 0 and all(dep in service_table.keys() for dep in tasks_list[0][2]):
-                    task_name, task_time, _ = tasks_list[0]
-                    node_cons.set_power(STRESS_CONSO)
-                    api.wait(task_time)
-                    node_cons.set_power(IDLE_CONSO)
-                    stress_time += task_time
-                    tasks_list.pop(0)
-                    service_table[task_name] = [api.node_id, {}]
-                    if len(tasks_list) == 0:
-                        termination_list[api.node_id] = True
-                    else:
-                        for task_name in tasks_list[0][2]:
-                            data_to_fetch.add(task_name)
+                        if task_name not in coordination_table.keys():
+                            content_to_fetch.add(task_name)
+                if t == "resp":
+                    content_sent, content_asked = content
+                    coordination_table.update(content_sent)
+                    for task_name in coordination_table.keys():
+                        if task_name in content_to_fetch:
+                            content_to_fetch.remove(task_name)
+                    content_to_send = {task_name: val for task_name, val in coordination_table.items() if task_name in content_asked}
+                    content_size = REQUEST_SIZE + COORDINATION_TABLE_ENTRY_SIZE * len(content_to_send.keys())
+                    api.send(INTERFACE_NAME, ("final", content_to_send), content_size, 0)
+                    for task_name in content_asked:
+                        if task_name not in coordination_table.keys():
+                            content_to_fetch.add(task_name)
+                    while len(tasks_list) > 0 and all(dep in coordination_table.keys() for dep in tasks_list[0][2]):
+                        task_name, task_time, _ = tasks_list[0]
+                        node_cons.set_power(STRESS_CONSO)
+                        api.wait(task_time)
+                        node_cons.set_power(IDLE_CONSO)
+                        stress_time += task_time
+                        tasks_list.pop(0)
+                        coordination_table[task_name] = [api.node_id, {}]
+                        if len(tasks_list) == 0:
+                            termination_list[api.node_id] = True
+                        else:
+                            for task_name in tasks_list[0][2]:
+                                content_to_fetch.add(task_name)
+                if t == "final":
+                    coordination_table.update(content)
+                    for task_name in coordination_table.keys():
+                        if task_name in content_to_fetch:
+                            content_to_fetch.remove(task_name)
+                    while len(tasks_list) > 0 and all(dep in coordination_table.keys() for dep in tasks_list[0][2]):
+                        task_name, task_time, _ = tasks_list[0]
+                        node_cons.set_power(STRESS_CONSO)
+                        api.wait(task_time)
+                        node_cons.set_power(IDLE_CONSO)
+                        stress_time += task_time
+                        tasks_list.pop(0)
+                        coordination_table[task_name] = [api.node_id, {}]
+                        if len(tasks_list) == 0:
+                            termination_list[api.node_id] = True
+                        else:
+                            for task_name in tasks_list[0][2]:
+                                content_to_fetch.add(task_name)
             code, data = api.receivet(INTERFACE_NAME, timeout=max(0, upt_end - api.read("clock")))
         upt_time += UPT_DURATION
     api.turn_off()
--- anticipation/tests_expe.py
+++ anticipation/tests_expe.py
@@ -1,12 +1,14 @@
+from concurrent.futures import ProcessPoolExecutor
+
 from run_simulation import run_expe
 import inspect
 
 
 class T():
-    def test_pull(self):
+    def test_pull_nb_deps_seq_1(self):
         parameters = {
             "type_comms": "pull",
-            "nb_deps_seq": 2,
+            "nb_deps_seq": 1,
             "deps_pos": "place",
             "chains_length": 2,
             "nb_chains": 1,
@@ -17,8 +19,56 @@ class T():
         assert metrics_per_node["upt_count"]["total"] == 25, metrics_per_node["upt_count"]["total"]
         print("passed")
 
+    def test_pull_nb_deps_seq_2(self):
+        parameters = {
+            "type_comms": "pull",
+            "nb_deps_seq": 2,
+            "deps_pos": "place",
+            "chains_length": 2,
+            "nb_chains": 1,
+            "topology": "clique",
+            "id_run": 0
+        }
+        metrics_per_node = run_expe(parameters)
+        assert metrics_per_node["upt_count"]["total"] == 53, metrics_per_node["upt_count"]["total"]
+        print("passed")
+
+    def test_pull_nb_deps_seq_3(self):
+        exit()
+        parameters = {
+            "type_comms": "pull",
+            "nb_deps_seq": 3,
+            "deps_pos": "place",
+            "chains_length": 2,
+            "nb_chains": 1,
+            "topology": "clique",
+            "id_run": 0
+        }
+        metrics_per_node = run_expe(parameters)
+        assert metrics_per_node["upt_count"]["total"] == 105, metrics_per_node["upt_count"]["total"]
+        print("passed")
+
+    def test_pull_chains_length_5(self):
+        parameters = {
+            "type_comms": "pull",
+            "nb_deps_seq": 1,
+            "deps_pos": "place",
+            "chains_length": 5,
+            "nb_chains": 1,
+            "topology": "clique",
+            "id_run": 0
+        }
+        metrics_per_node = run_expe(parameters)
+        assert metrics_per_node["upt_count"]["total"] == 25, metrics_per_node["upt_count"]["total"]
+        print("passed")
+
 
 if __name__ == "__main__":
-    for k, v in inspect.getmembers(T(), predicate=inspect.ismethod):
-        print(k)
-        v()
+    results = []
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        for k, v in inspect.getmembers(T(), predicate=inspect.ismethod):
+            print(k)
+            results.append(executor.submit(v))
+
+    for r in results:
+        print(r.result())
